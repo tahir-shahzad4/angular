a949d26298920bc7f9664931d8c480815baf00bb
tshah0107 <tahir.shahzad4@gmail.com>
2015-12-29 10:24:55 +0500

    examples2 cart table

---
+++ js/cartController.js
@@ -0,0 +1,30 @@
+$mainApp = angular.module("mainApp",[]);
+$mainApp.controller("CartController", ['$scope', function ($scope) {
+    $scope.invoice = {
+        items: [{
+            qty: 10,
+            description: 'item',
+            cost: 9.95}]
+    };
+
+    $scope.addItem = function() {
+        $scope.invoice.items.push({
+            qty: 1,
+            description: '',
+            cost: 0
+        });
+    },
+
+    $scope.removeItem = function(index) {
+        $scope.invoice.items.splice(index, 1);
+    },
+
+    $scope.total = function() {
+        var total = 0;
+        angular.forEach($scope.invoice.items, function(item) {
+            total += item.qty * item.cost;
+        })
+
+        return total;
+    }
+}]);
